daf1da06e1ecc9c33e05bbfaae5aa9bd727cf807
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-12 15:29:35 +0200

    Add simple Logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 console.log('JS OK');
+
+//! PRELIMINARI
+//# Recupero gli elementi dal DOM
+const cardElement = document.querySelector('ul');
+
+//# Inizializzo l'array
+const teamList = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+//# LOGICA
+
+let teamPage = '';
+
+for (let i = 0; i < teamList.length; i++) {
+  team = teamList[i];
+  console.table(team);
+  teamPage += `
+  <li>
+    <p>${team.image}</p>
+    <p>${team.name}</p>
+    <p>${team.role}</p>
+  </li>
+`;
+}
+
+cardElement.innerHTML = teamPage;
